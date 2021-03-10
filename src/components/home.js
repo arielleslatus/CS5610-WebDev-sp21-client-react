b0f788fa64f72d85c33bcac099c705d5ff9b27c0
AriellesMacBook <arielleslatus@login.khoury.neu.edu>
2021-03-10 14:12:54 -0500

    added my name

--- src/components/home.js
+++ src/components/home.js
@@ -4,15 +4,13 @@ import {Link} from "react-router-dom";
 const Home = () =>
     <>
         <h1>Home</h1>
+        <h2>WhiteBoard - Arielle Slatus</h2>
         <div className="list-group">
             <Link to="/courses/table" className="list-group-item">
                 Courses Table
             </Link>
             <Link to="/courses/grid" className="list-group-item">
                 Courses Grid
-            </Link>
-            <Link to="/editor" className="list-group-item">
-                Course Editor
             </Link>
         </div>
     </>
